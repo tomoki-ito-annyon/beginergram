f09a5dad328462093e6c2e74d63fe312e1cef0f9
tomoki-ito-annyon <s.m_soccer14@ezweb.ne.jp>
2021-06-27 04:40:13 +0900

    詳細表示機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+
+  before_action :set_tweet, only: [:edit, :show]
   def index
     @tweets = Tweet.all
     @tweets = Tweet.limit(10).order('created_at DESC')
@@ -18,16 +20,22 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
   end
+
+  def show
+  end
   
   private
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
